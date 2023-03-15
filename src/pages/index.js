4059108ebfe2f2256ecc0af40f4dc9651d483acd
nicober1 <s00889@newday.co.uk>
2023-03-15 20:03:25 +0530

    ss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
-import React from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-
+import React from 'react'
+import clsx from 'clsx'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout>
-      <div>
-        <h1 className="uppercase">Fluent Blogs</h1>
-        <a>https://chat.openai.com/</a>
-      </div>
-    </Layout>
-  );
+    const { siteConfig } = useDocusaurusContext()
+    return (
+        <Layout>
+            <div>
+                <h1 className="text-center text-4xl font-extrabold tracking-tight text-slate-900 dark:text-white sm:text-5xl lg:text-6xl">
+                    Fluent Blogs
+                </h1>
+                <a>https://chat.openai.com/</a>
+            </div>
+        </Layout>
+    )
 }
