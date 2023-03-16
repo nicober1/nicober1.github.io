79b208a8c6be9edd105a68ca7e7c043fab4b77fe
nicober1 <s00889@newday.co.uk>
2023-03-16 22:14:50 +0530

    s

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,24 +8,14 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <Layout noFooter wrapperClassName="home-page">
-      <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-        <img
-          src="https://play.tailwindcss.com/img/beams.jpg"
-          alt=""
-          class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
-          width="1308"
-        />
+      <div class="relative flex min-h-screen flex-col justify-center overflow-hidden">
         <h1 className="text-center text-4xl font-extrabold tracking-tight text-slate-900 dark:text-white sm:text-5xl lg:text-6xl">
           Fluent Blogs
         </h1>
 
         <div class="mt-6 flex justify-center space-x-6 text-sm sm:mt-10">
-          <a
-            class=""
-            target="_blank"
-            href="https://chat.openai.com/"
-          >
-            https://chat.openai.com/
+          <a target="_blank" href="https://chat.openai.com/">
+            ChatGPT
           </a>
         </div>
       </div>
