c8a7e7ce764d3501888ceed2f706bf89a6a67ce1
nicober.mani <nicobermani@outlook.com>
2023-03-16 22:00:51 +0530

    ss

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,20 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <Layout noFooter wrapperClassName="home-page">
-      <div className="relative mx-auto max-w-5xl pt-20 sm:pt-24 lg:pt-32">
+      <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+        <img
+          src="https://play.tailwindcss.com/img/beams.jpg"
+          alt=""
+          class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
+          width="1308"
+        />
         <h1 className="text-center text-4xl font-extrabold tracking-tight text-slate-900 dark:text-white sm:text-5xl lg:text-6xl">
           Fluent Blogs
         </h1>
 
         <div class="mt-6 flex justify-center space-x-6 text-sm sm:mt-10">
           <a
-            class="dark:highlight-white/20 flex h-12 w-full items-center 
-                        justify-center rounded-lg bg-slate-900 px-6 font-semibold text-white
-                         hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 
-                         focus:ring-offset-2 focus:ring-offset-slate-50 dark:bg-sky-500 dark:hover:bg-sky-400 sm:w-auto"
+            class=""
             target="_blank"
             href="https://chat.openai.com/"
           >
