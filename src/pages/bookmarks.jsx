70e8dbbb098f7ccaf3ada80d64dc5ea01b8133f7
nicober1 <s00889@newday.co.uk>
2023-03-18 01:54:14 +0530

    ss

--- src/pages/bookmarks.jsx
+++ src/pages/bookmarks.jsx
@@ -4,9 +4,9 @@ import BookmarkPage from '@site/src/components/BookmarkPage'
 
 const videoIds = [
   {url: 'https://chat.openai.com', title: 'ChatGPT', description: 'Chatbot using Artificial Intelligence'},
-  {url: 'https://chat.openai.com', title: 'ChatGPT', description: 'Artificial Intelligence'},
-  {url: 'https://chat.openai.com', title: 'ChatGPT', description: 'Artificial Intelligence'},
-  {url: 'https://chat.openai.com', title: 'ChatGPT', description: 'Artificial Intelligence'},
+  {url: 'https://www.youtube.com/', title: 'YouTube', description: 'Video Platform'},
+  {url: 'https://edition.cnn.com/', title: 'CNN', description: 'News'},
+  {url: 'https://analytics.google.com/', title: 'Google Analytics', description: 'Analytics'},
 ]
 export default function Home() {
   return (
