abd4a93ddf640c829845f5b4650bcfe2eb532251
nicober1 <s00889@newday.co.uk>
2023-03-17 22:59:31 +0530

    ss

---
+++ src/components/YouTubePlayer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const YouTubePlayer = ({videoIds}) => {
+  return (
+    <div className='flex flex-wrap'>
+      {videoIds.map((videoId) => (
+        <div className='w-full p-4 md:w-1/2 lg:w-1/3' key={videoId}>
+          <iframe
+            width='560'
+            height='315'
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title={`YouTube video player for video with ID ${videoId}`}
+            frameborder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;web-share'
+            allowFullScreen></iframe>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default YouTubePlayer
--- src/pages/live.jsx
+++ src/pages/live.jsx
@@ -1,26 +1,22 @@
 import React from 'react'
 import Layout from '@theme/Layout'
+import YouTubePlayer from '@site/src/components/YouTubePlayer'
+
+const videoIds = [
+  'JEg11Ps8w4M',
+  '5F1Eyf5QN3o',
+  '9Auq9mYxFEE',
+  '0ThMultL4PY',
+  'sYZtOFzM78M',
+  'gCNeDWCI0vo',
+  '9NyxcX3rhQs',
+  'h3MuIUNCCzI',
+]
 
 export default function Home() {
   return (
-    <Layout noFooter wrapperClassName='livenews-page'>
-      <div class='flex-row space-x-5'>
-        <iframe width='500' height='300' src='https://www.youtube.com/embed/JEg11Ps8w4M'></iframe>
-        <iframe width='400' height='400' src='https://www.youtube.com/embed/5F1Eyf5QN3o'></iframe>
-        <iframe width='400' height='400' src='https://www.youtube.com/embed/9Auq9mYxFEE'></iframe>
-        <iframe width='400' height='400' src='https://www.youtube.com/embed/0ThMultL4PY'></iframe>
-        <iframe width='400' height='400' src='https://www.youtube.com/embed/sYZtOFzM78M'></iframe>
-        <iframe width='400' height='400' src='https://www.youtube.com/embed/gCNeDWCI0vo'></iframe>
-        <iframe width='400' height='400' src='https://www.youtube.com/embed/9NyxcX3rhQs'></iframe>
-        <iframe
-          width='560'
-          height='315'
-          src='https://www.youtube.com/embed/h3MuIUNCCzI'
-          title='YouTube video player'
-          frameborder='0'
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          allowfullscreen></iframe>
-      </div>
+    <Layout noFooter wrapperClassName='live-page'>
+      <YouTubePlayer videoIds={videoIds} />
     </Layout>
   )
 }
