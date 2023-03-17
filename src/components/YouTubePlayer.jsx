f49d0daac7c83f62ea7131b5ae50c940ccee4321
nicober1 <s00889@newday.co.uk>
2023-03-18 00:14:00 +0530

    fs

--- src/components/YouTubePlayer.jsx
+++ src/components/YouTubePlayer.jsx
@@ -6,12 +6,12 @@ const YouTubePlayer = ({videoIds}) => {
       {videoIds.map((videoId) => (
         <div className='w-full p-4 md:w-1/2 lg:w-1/3' key={videoId}>
           <iframe
-            width='560'
+            width='100%'
             height='315'
             src={`https://www.youtube.com/embed/${videoId}`}
             title={`YouTube video player for video with ID ${videoId}`}
             frameborder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;web-share'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
             allowFullScreen></iframe>
         </div>
       ))}
