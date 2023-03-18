30090f15bcd50b8a2a8794dab8ec1d1d2af6d2ea
nicober1 <s00889@newday.co.uk>
2023-03-18 22:14:05 +0530

    ss

--- src/pages/live.jsx
+++ src/pages/live.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import Layout from '@theme/Layout'
 import YouTubePlayer from '@site/src/components/YouTubePlayer'
 
-const videoIds = [
-  'JEg11Ps8w4M',
-  '5F1Eyf5QN3o',
-  '9Auq9mYxFEE',
-  '0ThMultL4PY',
-  'sYZtOFzM78M',
-  'gCNeDWCI0vo',
-  '9NyxcX3rhQs',
-  'h3MuIUNCCzI',
+
+const videos = [
+  {id: 'h3MuIUNCCzI', title: 'France 24'},
+  {id: '5F1Eyf5QN3o', title: 'WION'},
+  {id: '9Auq9mYxFEE', title: 'SKY NEWS'},
+  {id: '0ThMultL4PY', title: 'National Geographic'},
+  {id: 'sYZtOFzM78M', title: 'India Today'},
+  {id: 'gCNeDWCI0vo', title: 'Al Jazeera'},
+  {id: '9NyxcX3rhQs', title: 'CNBC'},
 ]
 
 export default function Home() {
   return (
     <Layout noFooter wrapperClassName='live-page'>
       <div className='container mx-auto'>
-        <YouTubePlayer videoIds={videoIds} />
+        <YouTubePlayer videos={videos} />
       </div>
     </Layout>
   )
