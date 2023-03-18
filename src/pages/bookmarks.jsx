4c2a49f5a585aa2488b9c83807210725f96cfd71
nicober1 <s00889@newday.co.uk>
2023-03-18 17:25:56 +0530

    ss

--- src/pages/bookmarks.jsx
+++ src/pages/bookmarks.jsx
@@ -4,9 +4,14 @@ import BookmarkPage from '@site/src/components/BookmarkPage'
 
 const videoIds = [
   {url: 'https://chat.openai.com', title: 'ChatGPT', description: 'Chatbot using Artificial Intelligence'},
-  {url: 'https://www.youtube.com/', title: 'YouTube', description: 'Video Platform'},
-  {url: 'https://edition.cnn.com/', title: 'CNN', description: 'News'},
-  {url: 'https://analytics.google.com/', title: 'Google Analytics', description: 'Analytics'},
+  {url: 'https://www.youtube.com', title: 'YouTube', description: 'Video Platform'},
+  {url: 'https://edition.cnn.com', title: 'CNN', description: 'News'},
+  {url: 'https://analytics.google.com', title: 'Google Analytics', description: 'Analytics'},
+  {
+    url: 'https://restcountries.com',
+    title: 'REST COUNTRIES',
+    description: 'Get information about countries via a RESTful API',
+  },
 ]
 export default function Home() {
   return (
---
+++ src/pages/countries.jsx
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react'
+import Layout from '@theme/Layout'
+import axios from 'axios'
+import moment from 'moment-timezone'
+
+export default function () {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then((response) => {
+      const sortedCountries = response.data.sort((a, b) => a.name.common.localeCompare(b.name.common))
+      setCountries(sortedCountries).catch((error) => console.log(error))
+    })
+  }, [])
+
+  const getTimeByTimeZone = (timeZone) => {
+    const gmtOffset = parseInt(timeZone.substring(4, 6), 10)
+    const localTime = moment.utc().add(gmtOffset, 'hours')
+    return localTime.format('hh:mm:ss A')
+  }
+  return (
+    <Layout noFooter wrapperClassName='countries-page'>
+      <div className='container mx-auto'>
+        <h1 className='mb-4 text-4xl font-bold '>Countries of the World</h1>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+          {countries.map((country) => {
+            return (
+              <div key={country.cca3} className='overflow-hidden rounded-lg bg-white shadow-lg'>
+                <img src={country.flags.svg} alt={`${country.name.common} flag`} className='h-48 w-full border-4 border-solid object-cover' />
+                <div className='p-4'>
+                  <h3 className='text-lg font-semibold text-gray-800'>{country.name.common}</h3>
+                  <div className='mt-2'>
+                    <span className='text-gray-600'>Capital:</span> <span className='font-semibold text-gray-900'>{country.capital}</span>
+                  </div>
+                  <div className='mt-2'>
+                    <span className='text-gray-600'>Region:</span> <span className='font-semibold text-gray-900'>{country.region}</span>
+                  </div>
+                  <div className='mt-2'>
+                    <span className='text-gray-600'>Population:</span> <span className='font-semibold text-gray-900'>{country.population.toLocaleString()}</span>
+                  </div>
+                  <div className='mt-2'>
+                    <span className='text-gray-600'>Local Time:</span> <span className='font-semibold text-gray-900'>{getTimeByTimeZone(country.timezones[0])}</span>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </Layout>
+  )
+}
