2c36a7f18004147cf7d94679cda5f5f358e77d6b
nicober1 <s00889@newday.co.uk>
2023-03-21 13:18:16 +0530

    ss

--- src/components/DonateButton.jsx
+++ src/components/DonateButton.jsx
@@ -12,7 +12,7 @@ const DonateButton = () => {
     )}&body=${encodeURIComponent(body)}`
   }
   return (
-    <div className='flex  text-sm mr-4'>
+    <div className='mr-4  flex text-sm'>
       <button
         className='mr-2 inline-flex items-center rounded-full border border-transparent bg-indigo-600 px-4 py-2   text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
         onClick={handleClick}>
