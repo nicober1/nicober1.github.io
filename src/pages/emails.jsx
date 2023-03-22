8f8823f8237a0db19902f828f41995fb0dbadc11
nicober1 <s00889@newday.co.uk>
2023-03-22 18:10:47 +0530

    ss

--- src/pages/emails.jsx
+++ src/pages/emails.jsx
@@ -4,16 +4,22 @@ import {faker} from '@faker-js/faker'
 
 function generateEmails() {
   const emailIds = []
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 100; i++) {
     emailIds.push(faker.internet.email(undefined, undefined, 'gmail.com'))
   }
-  return emailIds.join(', ')
+  return emailIds
 }
 
 export default function Home() {
   return (
-    <div>
-      <p>{generateEmails()}</p>
-    </div>
+    <>
+      {generateEmails().map((email) => {
+        return (
+          <div key={email}>
+            <p>{email},</p>
+          </div>
+        )
+      })}
+    </>
   )
 }
