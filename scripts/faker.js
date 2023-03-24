256cf2cca62c9312af40e9ad094d362a78608335
nicober1 <s00889@newday.co.uk>
2023-03-24 18:10:52 +0530

    ss

--- scripts/faker.js
+++ scripts/faker.js
@@ -1,24 +1,15 @@
-const faker = require('@faker-js/faker')
+const {faker} = require('@faker-js/faker')
 const fs = require('fs')
 
-// Set the number of keywords to generate
-const numKeywords = 10000
+const numKeywords = 100
 
-// Generate the keywords using Faker.js
 const keywords = Array.from(
   {length: numKeywords},
-  () => faker.hacker.adjective() + ' ' + faker.hacker.noun(),
+  () => faker.word.adjective() + ' ' + faker.word.noun(),
 )
 
-function generateEmails() {
-  const emailIds = []
-  for (let i = 0; i < 100; i++) {
-    emailIds.push(faker.internet.email(undefined, undefined, 'gmail.com'))
-  }
-  return emailIds
-}
 
-// Write the list of keywords to a file
+
 fs.writeFile('./static/data/temp/keywords.txt', keywords.join(','), (err) => {
   if (err) {
     console.error(err)
