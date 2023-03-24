ddb1fba01ac8f2957182e0ef56bbfdb88c9655c1
nicober1 <s00889@newday.co.uk>
2023-03-24 11:52:26 +0530

    ss

--- src/pages/svggrid.jsx
+++ src/pages/svggrid.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import Layout from '@theme/Layout'
 import Svg1 from '@site/src/svg/1'
 import Svg2 from '@site/src/svg/2'
+import Svg3 from '@site/src/svg/3'
 
-const svgs = [Svg1, Svg2]
+const svgs = [Svg1, Svg2, Svg3]
 
 export default function Home() {
   return (
