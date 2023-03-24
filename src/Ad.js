cfef2d81ec9d51a970ef095682944fe31bbc5c12
nicober1 <s00889@newday.co.uk>
2023-03-24 16:56:16 +0530

    ss

---
+++ src/Ad.js
@@ -0,0 +1,52 @@
+import React, {useEffect} from 'react'
+
+export const SqAd = () => {
+  useEffect(() => {
+    ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+  }, [])
+
+  return (
+    <ins
+      className='adsbygoogle'
+      style={{display: 'block'}}
+      data-ad-client='ca-pub-6812671644017150'
+      data-ad-slot='9874121403'
+      data-ad-format='auto'
+      data-full-width-responsive='true'
+    />
+  )
+}
+
+export const WiAd = () => {
+  useEffect(() => {
+    ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+  }, [])
+
+  return (
+    <ins
+      className='adsbygoogle'
+      style={{display: 'block'}}
+      data-ad-client='ca-pub-6812671644017150'
+      data-ad-slot='3224952578'
+      data-ad-format='auto'
+      data-full-width-responsive='true'
+    />
+  )
+}
+
+export const VeAd = () => {
+  useEffect(() => {
+    ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+  }, [])
+
+  return (
+    <ins
+      className='adsbygoogle'
+      style={{display: 'block'}}
+      data-ad-client='ca-pub-6812671644017150'
+      data-ad-slot='1382507420'
+      data-ad-format='auto'
+      data-full-width-responsive='true'
+    />
+  )
+}
--- src/pages/apod.jsx
+++ src/pages/apod.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Layout from '@theme/Layout'
 import NASAImage from '@site/src/components/NASAImage'
+import {VeAd,SqAd,WiAd} from '@site/src/Ad'
 
 export default function Home() {
   return (
     <Layout noFooter wrapperClassName='live-page'>
       <div className='container mx-auto'>
         <NASAImage />
+        <VeAd />
+        <SqAd />
+        <WiAd />
       </div>
     </Layout>
   )
