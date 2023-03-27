d148c7fa929da36c83df7b389ffef8b5eeaf2c10
nicober1 <s00889@newday.co.uk>
2023-03-27 13:32:33 +0530

    ss

--- src/pages/radio.jsx
+++ src/pages/radio.jsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect,useRef} from 'react'
 import axios from 'axios'
 import Layout from '@theme/Layout'
 import useBaseUrl from '@docusaurus/useBaseUrl'
-const useRadioData = (url) => {
+const useFetchRadioData = (url) => {
   const [radioData, setRadioData] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
@@ -26,6 +26,7 @@ const RADIO_COUNT = 100
 const RadioItem = ({radio}) => {
   const {stationuuid, name, country, language, url, homepage} = radio
   const [playing, setPlaying] = useState(false)
+  const audioRef = useRef(null)
   const togglePlay = () => {
     setPlaying(!playing)
     const audio = document.getElementById(stationuuid)
@@ -35,6 +36,7 @@ const RadioItem = ({radio}) => {
       audio.play()
     }
   }
+  
   const closeOverlay = () => {
     setPlaying(false)
     const audio = document.getElementById(stationuuid)
@@ -71,7 +73,7 @@ const RadioItem = ({radio}) => {
 }
 export default function RadioDetails() {
   const url = useBaseUrl('/data/radio.json')
-  const {radioData, loading, error} = useRadioData(url)
+  const {radioData, loading, error} = useFetchRadioData(url)
   const [searchTerm, setSearchTerm] = useState('')
   const [searchResults, setSearchResults] = useState([])
   useEffect(() => {
@@ -83,13 +85,13 @@ export default function RadioDetails() {
       setSearchResults(filteredResults)
     }
   }, [radioData, searchTerm])
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value)
+  const handleSearchChange = ({target: {value}}) => {
+    setSearchTerm(value)
   }
   return (
-    <Layout title='Radio Details'>
+    <Layout title='World Radio'>
       <main className='container mx-auto p-4'>
-        <h1 className='mb-8 text-center text-4xl font-bold'>Radio Details</h1>
+        <h1 className='mb-8 text-center text-4xl font-bold'>World Radio</h1>
         <div className='mb-4'>
           <label htmlFor='searchInput' className='sr-only'>
             Search for radio stations
@@ -98,12 +100,19 @@ export default function RadioDetails() {
             type='text'
             id='searchInput'
             className='w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
-            placeholder='Search by name or country...'
+            placeholder='Search by Name or Country...'
             value={searchTerm}
             onChange={handleSearchChange}
           />
         </div>
-        {loading && <div className='text-center text-xl font-semibold'>Loading...</div>}
+        {loading && (
+          <div className='flex h-full w-full items-center justify-center'>
+            <svg className='h-12 w-12 animate-spin text-gray-400' viewBox='0 0 24 24'>
+              <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' fill='none' />
+              <path className='opacity-75' fill='currentColor' d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm16 0a8 8 0 01-8 8v4a10 10 0 0010-10h-4zm-8 4a4 4 0 100-8 4 4 0 000 8z' />
+            </svg>
+          </div>
+        )}
         {error && <div className='text-center text-xl font-semibold text-red-500'>{error.message}</div>}
         {searchResults && searchResults.length > 0 && (
           <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3'>
@@ -112,7 +121,7 @@ export default function RadioDetails() {
             ))}
           </div>
         )}
-        {searchResults && searchResults.length === 0 && <div className='text-center text-xl font-semibold'>No radio stations found</div>}
+        {searchResults && searchResults.length === 0 && <div className='text-center text-xl font-semibold'>Sorry, we couldn't find any radio stations matching your search term.</div>}
       </main>
     </Layout>
   )
--- src/pages/radio1.jsx
+++ src/pages/radio1.jsx
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import axios from 'axios'
 import Layout from '@theme/Layout'
 import useBaseUrl from '@docusaurus/useBaseUrl'
+
 const useRadioData = (url) => {
   const [radioData, setRadioData] = useState([])
   const [loading, setLoading] = useState(false)
@@ -22,103 +23,70 @@ const useRadioData = (url) => {
   }, [url])
   return {radioData, loading, error}
 }
+
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url))
+  const [playing, setPlaying] = useState(false)
+
+  const togglePlay = useCallback(() => {
+    setPlaying(!playing)
+  }, [playing])
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause()
+  }, [playing])
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false))
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false))
+    }
+  }, [])
+
+  return [playing, togglePlay]
+}
+
 const RADIO_COUNT = 100
+
 const RadioItem = ({radio}) => {
   const {stationuuid, name, country, language, url, homepage} = radio
-  // Use a state variable to track the playing status of the audio
-  const [playing, setPlaying] = useState(false)
-  // Define a function to toggle the playing state and update the audio element
-  const togglePlay = () => {
-    setPlaying(!playing)
-    const audio = document.getElementById(stationuuid)
-    if (playing) {
-      audio.pause()
-    } else {
-      audio.play()
-    }
-  }
-  // Define a function to close the overlay and pause the audio
-  const closeOverlay = () => {
-    setPlaying(false)
-    const audio = document.getElementById(stationuuid)
-    audio.pause()
-  }
+  const [playing, togglePlay] = useAudio(url)
+
+  const closeOverlay = useCallback(() => {
+    togglePlay()
+  }, [togglePlay])
+
   return (
-    <div
-      key={stationuuid}
-      className={`rounded-lg border bg-gradient-to-br ${
-        playing ? 'from-yellow-400 via-red-500 to-pink-600' : 'from-green-400 via-blue-500 to-purple-600'
-      } relative p-4 text-white shadow-lg transition-shadow hover:shadow-xl`}>
-      <div className='truncate text-xl font-bold'>{name}</div>
-      <div className='text-lg'>{country}</div>
-      <div className='uppercase'>{language}</div>
-      <div className='mt-4 flex items-center justify-between'>
-        <audio id={stationuuid} src={url}></audio>
-        <button onClick={togglePlay} className={`rounded-full bg-white p-2 text-black shadow-lg transition-shadow hover:shadow-xl ${playing ? 'animate-pulse' : ''}`}>
-          {playing ? 'Pause' : 'Play'}
-        </button>
-        <a href={homepage} target='_blank' rel='noreferrer' className='ml-4 flex-shrink-0'>
-          <img src={`https://www.google.com/s2/favicons?sz=64&domain=${homepage}`} alt={`${name} favicon`} className='h-10 w-10 rounded-full border-2 border-white' />
-        </a>
-      </div>
-      {/* Add an overlay div to show the station name when playing */}
-      {playing && (
-        <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-3xl font-bold'>
-          {name}
-          {/* Add a close button to hide the overlay */}
-          <button onClick={closeOverlay} className='absolute top-2 right-2 rounded-full bg-white p-2 text-black shadow-lg transition-shadow hover:shadow-xl'>
-            X
+    <>
+      <div
+        className={`rounded-lg border bg-gradient-to-br ${
+          playing ? 'from-yellow-400 via-red-500 to-pink-600' : 'from-green-400 via-blue-500 to-purple-600'
+        } relative px-4 py-2 text-white shadow-lg transition-shadow hover:shadow-xl sm:px-6 sm:py-4 md:p-4`}>
+        <div className='max-w-xs truncate text-xl font-bold sm:max-w-none sm:truncate'>{name}</div>
+        <div className='text-lg'>{country}</div>
+        <div className='uppercase'>{language}</div>
+        <div className='mt-4 flex items-center justify-between'>
+          <button onClick={togglePlay} className={`rounded-full bg-white p-2 text-black shadow-lg transition-shadow hover:shadow-xl ${playing ? 'animate-pulse' : ''}`}>
+            {playing ? 'Stop' : 'Play'}
           </button>
+          <a href={homepage} target='_blank' rel='noreferrer' className='underline'>
+            Visit Website
+          </a>
+        </div>
+      </div>
+      {playing && (
+        <div onClick={closeOverlay} className='fixed inset-0 flex cursor-pointer items-center justify-center bg-black bg-opacity-50'>
+          <div className='rounded-lg bg-white p-4 shadow-lg'>
+            <h2 className='text-xl font-bold'>Now Playing</h2>
+            <p className='text-lg'>{name}</p>
+            <p className='text-sm'>{country}</p>
+            <p className='text-sm'>{language}</p>
+            <button onClick={closeOverlay} className='mt-4 rounded-full bg-red-500 p-2 text-white shadow-lg transition-shadow hover:shadow-xl'>
+              Close
+            </button>
+          </div>
         </div>
       )}
-    </div>
-  )
-}
-export default function RadioDetails() {
-  const url = useBaseUrl('/data/radio.json')
-  const {radioData, loading, error} = useRadioData(url)
-  const [searchTerm, setSearchTerm] = useState('')
-  const [searchResults, setSearchResults] = useState([])
-  useEffect(() => {
-    if (radioData && radioData.length > 0) {
-      const filteredResults = radioData.filter((radio) => {
-        const searchRegex = new RegExp(searchTerm, 'i')
-        return searchRegex.test(radio.name) || searchRegex.test(radio.country)
-      })
-      setSearchResults(filteredResults)
-    }
-  }, [radioData, searchTerm])
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value)
-  }
-  return (
-    <Layout title='Radio Details'>
-      <main className='container mx-auto p-4'>
-        <h1 className='mb-8 text-center text-4xl font-bold'>Radio Details</h1>
-        <div className='mb-4'>
-          <label htmlFor='searchInput' className='sr-only'>
-            Search for radio stations
-          </label>
-          <input
-            type='text'
-            id='searchInput'
-            className='w-full rounded-lg border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
-            placeholder='Search by name or country...'
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-        </div>
-        {loading && <div className='text-center text-xl font-semibold'>Loading...</div>}
-        {error && <div className='text-center text-xl font-semibold text-red-500'>{error.message}</div>}
-        {searchResults && searchResults.length > 0 && (
-          <div className='grid grid-cols-3 gap-4'>
-            {searchResults.slice(0, RADIO_COUNT).map((radio) => (
-              <RadioItem key={radio.stationuuid} radio={radio} />
-            ))}
-          </div>
-        )}
-        {searchResults && searchResults.length === 0 && <div className='text-center text-xl font-semibold'>No radio stations found</div>}
-      </main>
-    </Layout>
+    </>
   )
 }
