57c2cbeec6e7027bb22e80888ebecbd9a2fff3ee
nicober1 <s00889@newday.co.uk>
2023-03-29 20:14:50 +0530

    ss

--- src/components/DonateButton.jsx
+++ src/components/DonateButton.jsx
@@ -7,14 +7,12 @@ const DonateButton = () => {
   const body = 'Hello developer, I have a question about your website.'
 
   function handleClick() {
-    window.location.href = `mailto:${email}?subject=${encodeURIComponent(
-      subject,
-    )}&body=${encodeURIComponent(body)}`
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
   }
   return (
-    <div className='mr-4 flex flex-wrap text-xs sm:text-sm'>
+    <div className='flex flex-wrap justify-end text-xs sm:text-sm'>
       <button
-        className='mr-1 inline-flex items-center rounded-full border border-transparent bg-indigo-600 px-4 py-2 text-white   shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mr-2'
+        className='mr-1 mb-1 inline-flex w-full items-center rounded-full border border-transparent bg-indigo-600 px-4 py-2 text-white shadow-sm   hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mr-2 sm:mb-0 sm:w-auto'
         onClick={handleClick}>
         <BeakerIcon className='mr-2 h-5 w-5' aria-hidden='true' />
         Contact developer
@@ -23,11 +21,15 @@ const DonateButton = () => {
         href='https://www.paypal.com/paypalme/fluentblogscom'
         target='_blank'
         rel='noopener noreferrer'
-        className='mr-1 inline-block rounded-full bg-green-600 py-2 px-4 text-white  hover:bg-green-700 sm:mr-2'>
+        className='mr-1 mb-1 inline-block w-full rounded-full bg-green-600 py-2 px-4 text-white hover:bg-green-700  sm:mr-2 sm:mb-0 sm:w-auto'>
         Support Dev
       </a>
 
-      <a href='https://www.buymeacoffee.com/FluentBlogs' target='_blank' rel='noopener noreferrer' className='rounded-full bg-yellow-500 py-2 px-4  text-white hover:bg-yellow-600'>
+      <a
+        href='https://www.buymeacoffee.com/FluentBlogs'
+        target='_blank'
+        rel='noopener noreferrer'
+        className='w-full rounded-full bg-yellow-500 py-2  px-4 text-white hover:bg-yellow-600 sm:w-auto'>
         Buy Developer a Coffee
       </a>
     </div>
