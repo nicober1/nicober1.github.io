3fa7ccad1f8589ae054f68589658d7d5058dd888
nicober1 <s00889@newday.co.uk>
2023-03-29 21:09:53 +0530

    ss

--- src/pages/cryptoprices.jsx
+++ src/pages/cryptoprices.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import axios from 'axios'
+
+
 import Layout from '@theme/Layout'
-import CryptoPrices from '@site/src/components/CryptoPrices'
+
+function CryptoPrices() {
+  const [data, setData] = useState([])
+  var url = useBaseUrl('/data/crypto.json')
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url)
+        setData(response.data.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div className='container mx-auto px-4 py-8'>
+      <h1 className='mb-8 text-center text-4xl font-bold'>Crypto Prices</h1>
+      <div className='grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-6'>
+        {data.map((coin) => (
+          <div key={coin.id} className='overflow-hidden rounded-lg bg-gray-200 shadow-md dark:bg-gray-600'>
+            <div className='p-4'>
+              <h2 className='mb-4 text-2xl font-bold'>{coin.name}</h2>
+              <p className='mb-2 font-bold text-cyan-600'>{coin.symbol}</p>
+              <p className='mb-4 text-lg font-bold'>${parseFloat(coin.priceUsd).toFixed(2)}</p>
+              <p className='mb-2 '>Market Cap</p>
+              <p className='mb-4 font-bold'>${parseFloat(coin.marketCapUsd).toLocaleString()}</p>
+              <p className='mb-2 '>24h Change</p>
+              <p className={`font-bold ${parseFloat(coin.changePercent24Hr) > 0 ? 'text-green-500' : 'text-red-500'}`}>
+                {parseFloat(coin.changePercent24Hr).toFixed(2)}%
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default function () {
   return (
     <Layout noFooter wrapperClassName='live-page'>
-        <CryptoPrices />
+      <CryptoPrices />
     </Layout>
   )
 }
--- src/pages/timezones.jsx
+++ src/pages/timezones.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import moment from 'moment-timezone'
+
 import Layout from '@theme/Layout'
-import Clock from '@site/src/components/Clock'
+
+const Clock = () => {
+  const [time, setTime] = useState(moment())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(moment())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div className='container mt-10  grid  gap-4 md:grid-cols-2 lg:grid-cols-6'>
+      {moment.tz
+        .names()
+        .reverse()
+        .map((city) => (
+          <div key={city} className='rounded bg-blue-200 p-4 shadow-md dark:bg-blue-900'>
+            <div className='text-lg font-medium text-cyan-800 dark:text-cyan-200'>{city}</div>
+            <div className='text-4xl font-bold'>{time.tz(city).format('HH:mm:ss')}</div>
+            <div className='text-sm text-cyan-900  dark:text-cyan-200'>{time.tz(city).format('dddd, D MMMM YYYY')}</div>
+            <div className='text-sm text-cyan-900 dark:text-cyan-200'>{time.tz(city).format('ha z')}</div>
+          </div>
+        ))}
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <Layout noFooter wrapperClassName='live-page'>
-      <h1 className='mt-10 dark:text-shadow-lg mx-auto text-center text-4xl font-bold text-black dark:text-white'>
+      <h1 className='dark:text-shadow-lg mx-auto mt-10 text-center text-4xl font-bold text-black dark:text-white'>
         Timezones
       </h1>
 
