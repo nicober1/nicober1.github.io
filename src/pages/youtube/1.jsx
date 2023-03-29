4f318a17d4086ed09a7abb06258d44c545ed1cca
nicober1 <s00889@newday.co.uk>
2023-03-29 17:54:20 +0530

    ss

---
+++ src/pages/youtube/1.jsx
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import ReactPlayer from 'react-player'
+import Layout from '@theme/Layout'
+const videoIds = ['9bZkp7q19f0', 'RgKAFK5djSk', 'fRh_vgS2dFE']
+const formatNumber = (num) => {
+  if (num < 1e9) return num
+  return (num / 1e9).toFixed(1) + ' Billion'
+}
+export default function App() {
+  const [videos, setVideos] = useState([])
+  useEffect(() => {
+    fetchData()
+  }, [])
+  async function fetchData() {
+    try {
+      const response = await axios.get('https://www.googleapis.com/youtube/v3/videos', {
+        params: {
+          part: 'statistics',
+          id: videoIds.join(','),
+          key: 'AIzaSyC7aNg_UBZlCHC13MHukveVKEcstbXruSg',
+        },
+      })
+      setVideos(response.data.items)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <Layout noFooter>
+    <div className='container mx-auto max-w-6xl'>
+      <h1 className='text-center text-4xl font-bold '>YouTube's Most Viewed Videos</h1>
+      <div className='flex flex-wrap justify-center'>
+        {videos.map((video) => {
+          const {id, statistics} = video
+          return (
+            <div className='m-4 w-full rounded border p-4 shadow-lg md:w-1/2 lg:w-1/3' key={id}>
+              <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} controls width='100%' height='100%' />
+              <h3 className='text-lg font-semibold'>Views: {formatNumber(statistics.viewCount)}</h3>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+    </Layout>
+  )
+}
