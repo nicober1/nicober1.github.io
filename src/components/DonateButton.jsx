2039bb0209c87cd8ea6b65626a28c8e423fda332
nicober1 <s00889@newday.co.uk>
2023-03-30 13:53:49 +0530

    ss

--- src/components/DonateButton.jsx
+++ src/components/DonateButton.jsx
@@ -10,19 +10,19 @@ const DonateButton = () => {
     window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`
   }
   return (
-    <div className='container mx-auto my auto flex items-center flex-wrap justify-end text-sm'>
+    <div className='container mx-auto my-auto flex items-center flex-wrap text-sm'>
       <button
         className='mr-1 mb-1 inline-flex  w-full items-center rounded-full border border-transparent bg-cyan-500 bg-opacity-40  px-4 py-2 text-white shadow-sm   hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mr-2 sm:mb-0 sm:w-auto'
         onClick={handleClick}>
         <BeakerIcon className='mr-2 h-5 w-5' aria-hidden='true' />
-        Contact developer
+        Contact Developer
       </button>
       <a
         href='https://www.paypal.com/paypalme/fluentblogscom'
         target='_blank'
         rel='noopener noreferrer'
         className='mr-1 mb-1 inline-block w-full rounded-full bg-green-600 bg-opacity-40 py-2 px-4 text-white hover:bg-green-700 sm:mr-2 sm:mb-0 sm:w-auto'>
-        Support Dev
+        Support Developer
       </a>
 
       <a
@@ -30,7 +30,7 @@ const DonateButton = () => {
         target='_blank'
         rel='noopener noreferrer'
         className='w-full rounded-full bg-yellow-500 bg-opacity-40  py-2 px-4 text-white hover:bg-yellow-600 sm:w-auto'>
-        Buy Developer a Coffee
+        Buy Developer Coffee
       </a>
     </div>
   )
