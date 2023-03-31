794a794bf8efb22258c613fd158e8284e9d6e851
nicober1 <s00889@newday.co.uk>
2023-03-31 13:24:15 +0530

    ss

--- src/pages/tw.jsx
+++ src/pages/tw.jsx
@@ -17,7 +17,11 @@ import {
 export default function SolarModel() {
   return (
     <Layout noFooter>
-      <div className='container mx-auto px-4'>
+      <div className='container mx-auto my-auto bg-gradient-to-r from-blue-400 to-red-600 py-5  px-5 text-center'>
+        <h1 className='text-4xl font-bold text-white'>Twitter Timelines</h1>
+        <p className='mt-4 text-xl text-white'>One-Stop place to get updates from Important Twitter Timelines</p>
+      </div>
+      <div className='container mx-auto mt-10 px-4'>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
           <Timeline
             dataSource={{
@@ -30,10 +34,10 @@ export default function SolarModel() {
             }}
           />
           <TwitterTimelineEmbed sourceType='profile' screenName='WhiteHouse' options={{height: 400, width: '100%'}} />
-          <TwitterTimelineEmbed sourceType='profile' screenName='WhiteHouse' options={{height: 400, width: '100%'}} />
-          <TwitterTimelineEmbed sourceType='profile' screenName='WhiteHouse' options={{height: 400, width: '100%'}} />
-          <TwitterTimelineEmbed sourceType='profile' screenName='WhiteHouse' options={{height: 400, width: '100%'}} />
-          <TwitterTimelineEmbed sourceType='profile' screenName='WhiteHouse' options={{height: 400, width: '100%'}} />
+          <TwitterTimelineEmbed sourceType='profile' screenName='JoeBiden' options={{height: 400, width: '100%'}} />
+          <TwitterTimelineEmbed sourceType='profile' screenName='POTUS' options={{height: 400, width: '100%'}} />
+          <TwitterTimelineEmbed sourceType='profile' screenName='elonmusk' options={{height: 400, width: '100%'}} />
+          <TwitterTimelineEmbed sourceType='profile' screenName='narendramodi' options={{height: 400, width: '100%'}} />
         </div>
       </div>
     </Layout>
