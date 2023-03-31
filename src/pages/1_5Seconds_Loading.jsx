0b90601e66adf71b1f9f842b1c0c7851ce5ab7a2
nicober1 <s00889@newday.co.uk>
2023-03-31 17:18:47 +0530

    ss

---
+++ src/pages/1_5Seconds_Loading.jsx
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react'
+import {Spinner} from 'react-bootstrap'
+import {BounceLoader} from 'react-spinners'
+
+const Loading = ({children}) => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 8000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
+  return (
+    <div>
+      <div
+        className={`flex h-screen items-center justify-center bg-gradient-to-r from-blue-400 to-purple-600 ${
+          loading ? '' : 'hidden'
+        }`}>
+        <BounceLoader color='#FFFFFF' size={150} />
+        <svg viewBox='0 0 100 100' width='100' height='100'>
+          <circle cx='50' cy='50' r='45' fill='none' stroke-width='10' stroke-dasharray='283' stroke-dashoffset='141.5'>
+            <animate attributeName='stroke-dashoffset' values='141.5;0;141.5' dur='1.5s' repeatCount='indefinite' />
+            <animate
+              attributeName='stroke'
+              values='#ff0000;#00ff00;#0000ff;#ff0000'
+              dur='1.5s'
+              repeatCount='indefinite'
+            />
+          </circle>
+        </svg>
+      </div>
+      <div className={`${loading ? 'hidden' : ''}`}>{children}</div>
+    </div>
+  )
+}
+
+export default Loading
--- src/pages/tw.jsx
+++ src/pages/tw.jsx
@@ -1,56 +1,35 @@
 import React, {useState, useEffect} from 'react'
-import {Spinner} from 'react-bootstrap'
 import Layout from '@theme/Layout'
-import {BounceLoader} from 'react-spinners'
 import {Timeline} from 'react-twitter-widgets'
 import {TwitterTimelineEmbed} from 'react-twitter-embed'
+import Loading from './1_5Seconds_Loading'
 
 const profiles = ['WhiteHouse', 'NASA', 'WHO']
 
 export default () => {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false)
-    }, 5000)
-
-    return () => {
-      clearTimeout(timer)
-    }
-  }, [])
   return (
     <Layout noFooter>
-      <div>
-        <div
-          className={`flex h-screen items-center justify-center bg-gradient-to-r from-blue-400 to-purple-600 ${
-            loading ? '' : 'hidden'
-          }`}>
-          <Spinner animation='border' />
-          <BounceLoader color='#FFFFFF' size={150} />
-        </div>
-        <div className={`${loading ? 'hidden' : ''}`}>
-          <>
-            <div className='container mx-auto mt-10 bg-gradient-to-r from-blue-400 to-red-600 text-center'>
-              <h1 className='text-4xl font-bold text-white'>Twitter Timelines</h1>
-              <p className='mt-4 text-xl text-white'>One-Stop place to get updates from Important Twitter Timelines</p>
+      <Loading>
+        <>
+          <div className='container mx-auto mt-10 bg-gradient-to-r from-blue-400 to-red-600 text-center'>
+            <h1 className='text-4xl font-bold text-white'>Twitter Timelines</h1>
+            <p className='mt-4 text-xl text-white'>One-Stop place to get updates from Important Twitter Timelines</p>
+          </div>
+          <div className='container mx-auto mt-10 px-4'>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+              {profiles.map((screenName) => (
+                <>
+                  <TwitterTimelineEmbed
+                    sourceType='profile'
+                    screenName={screenName}
+                    options={{height: 400, width: '100%'}}
+                  />
+                </>
+              ))}
             </div>
-            <div className='container mx-auto mt-10 px-4'>
-              <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
-                {profiles.map((screenName) => (
-                  <>
-                    <TwitterTimelineEmbed
-                      sourceType='profile'
-                      screenName={screenName}
-                      options={{height: 400, width: '100%'}}
-                    />
-                  </>
-                ))}
-              </div>
-            </div>
-          </>
-        </div>
-      </div>
+          </div>
+        </>
+      </Loading>
     </Layout>
   )
 }
