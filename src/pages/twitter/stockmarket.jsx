a19f2c1bec3ad19faa40f626b73e092f9409828b
nicober1 <s00889@newday.co.uk>
2023-04-05 16:22:03 +0530

    ss

---
+++ src/pages/twitter/stockmarket.jsx
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+import {Timeline} from 'react-twitter-widgets'
+import {TwitterTimelineEmbed} from 'react-twitter-embed'
+import Loading from '@site/src/pages/1Loading'
+import HeaderTypeWriter from '@site/src/components/HeaderTypeWriter'
+
+
+
+
+export default () => {
+  return (
+    <Loading>
+      <>
+        <div className='container mx-auto mt-10 dark:text-white  text-center'>
+          <HeaderTypeWriter>Twitter Timelines</HeaderTypeWriter>
+        </div>
+        <div className='container mx-auto mt-10 px-4'>
+          <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+            {profiles.map((screenName) => (
+              <>
+                <TwitterTimelineEmbed
+                  sourceType='profile'
+                  screenName={screenName}
+                  options={{height: 600, width: '100%'}}
+                />
+              </>
+            ))}
+          </div>
+        </div>
+      </>
+    </Loading>
+  )
+}
+
+const profiles = ['AlgoBoffin', 'vka27', 'CNBCTV18News', 'EconomicTimes', 'Bhagirathsutar', 'Am_Shai']
+
