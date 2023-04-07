1d1a09c2ed7cd9d487e2fbbb620436a814177953
nicober1 <s00889@newday.co.uk>
2023-04-07 14:27:43 +0530

    ss

--- src/pages/resume/1.jsx
+++ src/pages/resume/1.jsx
@@ -9,27 +9,31 @@ const Resume = ({name, email, phone, summary, skills, projects, education}) => {
         <div className='contact-info my-auto flex  space-x-4 '>
           <a
             href={`mailto:${email}`}
-            className='text-whiteduration-300 flex transform items-center transition-colors  hover:scale-110 hover:text-white'>
+            className='text-whiteduration-300 flex transform items-center text-white transition-colors hover:scale-110 hover:text-white'>
             <FaEnvelope className='h-6 w-6' />
             <span className='ml-2'>{email}</span>
           </a>
           <a
             href={`tel:${phone}`}
-            className='text-whiteduration-300 flex transform  items-center transition-colors  hover:scale-110 hover:text-white'>
+            className='text-whiteduration-300 flex transform  items-center text-white transition-colors  hover:scale-110 hover:text-white'>
             <FaPhone className='h-6 w-6' />
             <span className='ml-2'>{phone}</span>
           </a>
         </div>
       </header>
+      {/* Summary */}
       <section className='summary mt-4 text-white'>
         <h2 className='text-2xl font-bold'>Summary</h2>
         <p className='text-white'>{summary}</p>
       </section>
+      {/* Skills */}
       <section className='skills mt-4'>
         <h2 className='text-2xl font-bold'>Skills</h2>
         <ul className='flex flex-wrap gap-2'>
           {skills.map((skill) => (
-            <div key={skill} className='rounded-lg from-black via-blue-700 to-black px-2 py-1 p-2 text-white'>
+            <div
+              key={skill}
+              className='rounded-lg bg-gradient-to-br from-black via-blue-700 to-black p-2  text-white shadow-lg'>
               {skill}
             </div>
           ))}
