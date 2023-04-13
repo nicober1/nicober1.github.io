8158acd3b04619ad734e55e129f673c77888bb92
nicober1 <s00889@newday.co.uk>
2023-04-13 20:03:18 +0530

    ss

--- src/pages/nyt/hcfiction.jsx
+++ src/pages/nyt/hcfiction.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import books from '/data/nyt/bestsellerbooks.json'
-import BookPage from './BookPage'
+import BookPage from '@site/src/components/BookPage'
 
 const HcFiction = () => {
   return <BookPage books={books} header={'NewYork Times Best Seller - HardCover Fiction'} />
