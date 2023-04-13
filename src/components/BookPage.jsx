545be196b14449d8d02e940e021fd8c361ca340d
nicober1 <s00889@newday.co.uk>
2023-04-13 20:12:36 +0530

    ss

--- src/components/BookPage.jsx
+++ src/components/BookPage.jsx
@@ -7,8 +7,8 @@ const BookCard = ({book}) => {
     <CardGradientsDark>
       <a href={book.amazonLink} target='_blank' rel='noreferrer'>
         <div className='card flex flex-col overflow-hidden rounded-lg shadow-lg transition duration-300 ease-in-out hover:scale-105'>
-          <div className='relative h-64 w-full'>
-            <img src={book.bookImage} alt={book.bookTitle} className='absolute inset-0 h-full w-full object-cover' />
+          <div className='relative h-96 w-full'>
+            <img src={book.bookImage} alt={book.bookTitle} className='absolute inset-0 h-full w-full ' />
             <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-70'></div>
             <div className='absolute bottom-0 left-0 p-2 text-white'>
               <h3 className='text-lg font-bold'>{book.bookTitle}</h3>
@@ -16,10 +16,10 @@ const BookCard = ({book}) => {
             </div>
           </div>
           <div className='p-4 text-white'>
-            <p className='truncate text-sm'>{book.description}</p>
+            <p className='text-sm'>{book.description}</p>
             <div className='mt-2 flex items-center justify-between'>
-              <p className='text-xs'>{book.bookPublisher}</p>
-              <p className='text-xs'>Weeks on NYT Best Seller List: {book.weeksOnList}</p>
+              <p className='text-xs'>Publisher: {book.bookPublisher}</p>
+              <p className='text-xs'>{book.weeksOnList} Weeks on NYT Best Seller List</p>
             </div>
           </div>
         </div>
@@ -30,7 +30,7 @@ const BookCard = ({book}) => {
 
 const BookGrid = ({books}) => {
   return (
-    <div className='grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3'>
+    <div className='grid grid-cols-4 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
       {books.map((book) => (
         <BookCard key={book.bookTitle} book={book} />
       ))}
