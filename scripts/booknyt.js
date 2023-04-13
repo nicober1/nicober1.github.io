ff5e2e759cebd455f8ca9a1755bd38fb695c22e1
nicober1 <s00889@newday.co.uk>
2023-04-13 16:51:59 +0530

    ss

---
+++ scripts/booknyt.js
@@ -0,0 +1,88 @@
+const axios = require('axios')
+const fs = require('fs')
+const axiosRetry = require('axios-retry')
+
+// create an axios instance with a base URL
+const api = axios.create({
+  baseURL: 'https://api.nytimes.com/svc/books/v3/lists',
+})
+
+// attach axios-retry to the instance
+axiosRetry(api, {
+  retries: 5, // number of retries
+  retryDelay: (retryCount) => {
+    return retryCount * 60 * 1000 // wait for 1 minute between retries
+  },
+  retryCondition: (error) => {
+    // retry only if status code is 429 (Too Many Requests)
+    return error.response.status === 429
+  },
+})
+const NYT_API_KEY = 'tNErHGpGWt7JJR4A9sfJH2sMiYx7v4zq'
+const AMAZON_ASSOCIATE_TAG = 'fluentblogs-21'
+const LIST_NAME = 'hardcover-fiction'
+const getBestsellerBooks = async () => {
+  try {
+    const results = []
+    let currentDate = new Date()
+    const l = 30 // iterate over 52 weeks
+    const seenTitles = new Set() // use a Set to store seen titles
+    for (let i = 0; i < l; i++) {
+      let formattedWeek = currentDate.toISOString().slice(0, 10) // change variable name
+      const response = await api.get(
+        `/${formattedWeek}/${LIST_NAME}.json?api-key=${NYT_API_KEY}`,
+      )
+      const books = response.data.results.books
+      // use array methods instead of for loop
+      const filteredBooks = books
+        .map((book) => {
+          const publishDate = response.data.results.published_date ?? '' // use nullish coalescing operator
+          const bookTitle = book.title ?? '' // use nullish coalescing operator
+          const bookAuthor = book.author ?? '' // use nullish coalescing operator
+          const bookImage = book.book_image ?? '' // use nullish coalescing operator
+          const bookPublisher = book.publisher ?? '' 
+          const weeksOnList = book.weeks_on_list ?? '' 
+          const description = book.description ?? '' 
+          let amazonLink = book.amazon_product_url // use let instead of const
+          if (amazonLink) {
+            amazonLink = amazonLink.replace(/tag=[^&]+/, `tag=${AMAZON_ASSOCIATE_TAG}`)
+          } else {
+            amazonLink = '' // use empty string instead of null or undefined
+          }
+          if (!seenTitles.has(bookTitle)) {
+            // check if title has been seen before
+            seenTitles.add(bookTitle) // add title to the Set
+            return {
+              publishDate,
+              bookTitle,
+              bookAuthor,
+              bookImage,
+              bookPublisher,
+              description,
+              weeksOnList,
+              amazonLink,
+            }
+          }
+        })
+        .filter((book) => book) // remove any null or undefined values from the array
+      results.push(...filteredBooks) // use spread operator to flatten array
+      currentDate.setDate(currentDate.getDate() - 7) // subtract 7 days from current date
+    }
+    return results
+  } catch (error) {
+    console.error(error)
+  }
+}
+const saveBestsellerBooks = async () => {
+  try {
+    const bestsellerBooks = await getBestsellerBooks()
+    const bestsellerBooksJSON = JSON.stringify(bestsellerBooks, null, 2)
+    fs.writeFile('./static/data/nyt/bestsellerbooks.json', bestsellerBooksJSON, (error) => {
+      if (error) throw error
+      console.log('Bestseller books data saved to bestseller-books.json')
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+saveBestsellerBooks()
---
+++ src/pages/nyt.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import books from '/data/nyt/bestsellerbooks.json'
+import Loading from '@site/src/pages/1Loading'
+import CardGradientsDark from '@site/src/components/CardGradientsDark'
+
+const BookCard = ({book}) => {
+  return (
+    <CardGradientsDark>
+      <a href={book.amazonLink} target='_blank' rel='noreferrer'>
+        <div className='card flex  overflow-hidden rounded-lg  shadow-lg'>
+          <img src={book.bookImage} alt={book.bookTitle} className=' object-cover' />
+          <div className='p-4 text-white'>
+            <h3 className='text-lg font-bold '>{book.bookTitle}</h3>
+            <p className='text-xs '>{book.bookAuthor}</p>
+            <p className='text-xs '>{book.description}</p>
+            <p className='text-xs '>{book.bookPublisher}</p>
+            <p className='text-xs '>Weeks on NYT Best Seller List: {book.weeksOnList}</p>
+          </div>
+        </div>
+      </a>
+    </CardGradientsDark>
+  )
+}
+
+const BookGrid = ({books}) => {
+  return (
+    <div className='grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3'>
+      {books.map((book) => (
+        <BookCard key={book.bookTitle} book={book} />
+      ))}
+    </div>
+  )
+}
+
+const PageHeader = () => {
+  return (
+    <div className='bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-8 text-white'>
+      <h1 className='text-4xl font-bold'>NYT Bestseller List</h1>
+    </div>
+  )
+}
+
+const BookPage = () => {
+  return (
+    <Loading time={0}>
+      <div className='min-h-screen'>
+        <PageHeader />
+        <BookGrid books={books} />
+      </div>
+    </Loading>
+  )
+}
+
+export default BookPage
