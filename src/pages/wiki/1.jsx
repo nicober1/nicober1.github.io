0b574ef8b030e2bcf36aebae17af3aba77438626
nicober1 <s00889@newday.co.uk>
2023-04-17 16:18:39 +0530

    ss

--- src/pages/wiki/1.jsx
+++ src/pages/wiki/1.jsx
@@ -1,20 +1,16 @@
-// A React function to use Wikipedia search API and build an encyclopedia page
 import React, {useState, useEffect} from 'react'
-
-// A component to display a single Wikipedia result with title and snippet
+import './1.css'
 function WikiCard(props) {
-  // Assume props.result is an object with title, snippet and pageid properties
   return (
-    <div className='wiki-card' onClick={() => props.onSelect(props.result)}>
-      <h4>{props.result.title}</h4>
-      <p dangerouslySetInnerHTML={{__html: props.result.snippet}} />
+    <div
+      className='transform cursor-pointer rounded-md bg-white p-4 shadow-md transition duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg'
+      onClick={() => props.onSelect(props.result)}>
+      <h4 className='mb-2 text-lg font-medium text-black'>{props.result.title}</h4>
+      <p className='text-black' dangerouslySetInnerHTML={{__html: props.result.snippet}} />
     </div>
   )
 }
-
-// A component to display a list of Wikipedia results
 function WikiList(props) {
-  // Assume props.results is an array of objects with title, snippet and pageid properties
   return (
     <div className='wiki-list'>
       {props.results.map((result) => (
@@ -23,38 +19,24 @@ function WikiList(props) {
     </div>
   )
 }
-
-// A component to display a Wikipedia page with title and content
 function WikiPage(props) {
-  // Assume props.page is an object with title and content properties
   return (
     <div className='wiki-page'>
-      <h2>{props.page.title}</h2>
+      <h2 className='mb-4 text-2xl font-bold'>{props.page.title}</h2>
       <div dangerouslySetInnerHTML={{__html: props.page.content}} />
     </div>
   )
 }
-
-// A component to display an encyclopedia page with a search input and a list or a page
-function Encyclopedia(props) {
-  // A state variable to store the search query
+export default function Encyclopedia(props) {
   const [query, setQuery] = useState('')
-  // A state variable to store the search results
   const [results, setResults] = useState([])
-  // A state variable to store the selected page
   const [page, setPage] = useState(null)
-
-  // A function to handle the change of the search input
   function handleChange(event) {
     setQuery(event.target.value)
   }
-
-  // A function to handle the selection of a result
   function handleSelect(result) {
     setPage(result)
   }
-
-  // A function to fetch the search results from the Wikipedia API
   async function fetchResults() {
     try {
       const response = await fetch(
@@ -66,8 +48,6 @@ function Encyclopedia(props) {
       console.error(error)
     }
   }
-
-  // A function to fetch the page content from the Wikipedia API
   async function fetchPage() {
     try {
       const response = await fetch(
@@ -79,8 +59,6 @@ function Encyclopedia(props) {
       console.error(error)
     }
   }
-
-  // An effect hook to fetch the results when the query changes
   useEffect(() => {
     if (query) {
       fetchResults()
@@ -88,20 +66,21 @@ function Encyclopedia(props) {
       setResults([])
     }
   }, [query])
-
-  // An effect hook to fetch the page when the pageid changes
   useEffect(() => {
     if (page && !page.content) {
       fetchPage()
     }
   }, [page])
-
   return (
-    <div className='encyclopedia'>
-      <input type='text' value={query} onChange={handleChange} placeholder='Search Wikipedia' />
+    <div className='encyclopedia flex flex-col gap-4'>
+      <input
+        className='rounded border border-gray-300 p-2'
+        type='text'
+        value={query}
+        onChange={handleChange}
+        placeholder='Search Wikipedia'
+      />
       {page ? <WikiPage page={page} /> : <WikiList results={results} onSelect={handleSelect} />}
     </div>
   )
 }
-
-export default Encyclopedia
