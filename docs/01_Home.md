db3808c4449e1696195a7f4b46c44e781a35a489
nicober1 <s00889@newday.co.uk>
2023-04-27 17:27:47 +0530

    ss

--- docs/01_Home.md
+++ docs/01_Home.md
@@ -15,3 +15,10 @@ sidebar_position: 1
 [Bing AI](https://www.bing.com/search?form=MY0291&OCID=MY0291&q=Bing+AI&showconv=1)
 
 [FluentBlogs.com](https://fluentblogs.com)
+
+[GitHub](https://github.com/nicober1/nicober1.github.io)
+
+
+### Indian Stock Market Links:
+
+[FnO Stocks](https://fluentblogs.com/nse/fo)
--- src/pages/nse/fo.jsx
+++ src/pages/nse/fo.jsx
@@ -21,18 +21,24 @@ export default function App() {
   return (
     <div className='mt-10 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
       {data.map((item) => (
-        <div key={item.symbol} className={`${item.change > 0 ? 'bg-green-800' : 'bg-red-800'} mb-5 p-4 text-white`}>
-          <h2 className='mb-2 text-lg font-bold'>{item.symbol}</h2>
-          <p className='mb-2 '>
-            {item.meta.companyName} - {item.meta.industry}
+        <div
+          key={item.symbol}
+          className={`${item.change > 0 ? 'bg-green-800' : 'bg-red-800'} mb-5 rounded-md p-4 text-white`}>
+          <h2 className='mb-2 text-base font-bold'>{item.symbol}</h2>
+          <p className='mb-2 text-xs'>
+            {item.meta.companyName} {item.meta.industry}
           </p>
-          <div className='mb-2 grid grid-cols-2 gap-4'>
-            <div>
-              <p className=''>Last Price</p>
+          <div className='mt-10 text-xs'>
+            <div className='flex gap-4'>
+              <p className=''>Last Price: </p>
               <p className='font-bold'>{item.lastPrice}</p>
             </div>
-            <div>
-              <p className=''>Change</p>
+            <div className='flex gap-4'>
+              <p className=''>% Change: </p>
+              <p className='font-bold'>{item.pChange}</p>
+            </div>
+            <div className='flex gap-4'>
+              <p className=''>Change: </p>
               <p className={`font-bold ${item.change > 0 ? 'text-green-200' : 'text-red-200'}`}>{item.change}</p>
             </div>
           </div>
