da73fc5aac82c3a7680e6b6c6810a7cfb503ce2d
nicober1 <85498722+nicober1@users.noreply.github.com>
2023-05-06 10:12:57 +0000

    ss

--- python/main.py
+++ python/main.py
@@ -1,1 +1,28 @@
-print("Hello, world!")
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def fetch_bbc_headlines():
+    url = "https://www.bbc.com/news"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    headlines = soup.find("body").find_all("h3")
+    for x in headlines:
+        print(x.text.strip())
+    headliness = []
+    for element in headlines:
+        headliness.append(element.text.strip())
+    return headliness
+
+
+def write_to_json_file(data):
+    with open("bbc_headlines.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
+# Fetch the headlines
+headliness = fetch_bbc_headlines()
+
+# Write the headlines to a JSON file
+write_to_json_file(headliness)
