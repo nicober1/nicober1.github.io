4feb6abd87aa87630b61f46b812fa62a77c1914a
nicober1 <85498722+nicober1@users.noreply.github.com>
2023-05-06 15:13:42 +0000

    ss

--- python/bbc.py
+++ python/bbc.py
@@ -2,19 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://edition.cnn.com/"
+url = "https://bbc.com/"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-headlines = soup.find_all("span")
+headlines = soup.find_all("h3")
 
 filtered_headlines = []
 
 for headline in headlines:
     headline_text = headline.get_text().strip()
-    if headline_text and len(headline_text) > 25 and "CNN" not in headline_text and "cnn" not in headline_text:
+    if headline_text and len(headline_text) > 25 and "BBC" not in headline_text and "bbc" not in headline_text:
         filtered_headlines.append(headline_text)
         print(headline_text)
 
-with open("./static/scrap/cnn.json", "w", encoding="utf-8") as f:
+with open("./static/scrap/bbc.json", "w", encoding="utf-8") as f:
     json.dump(filtered_headlines, f, indent=4, ensure_ascii=False)
--- python/cnbc.py
+++ python/cnbc.py
@@ -2,19 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://edition.cnn.com/"
+url = "https://cnbc.com/"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-headlines = soup.find_all("span")
+headlines = soup.find_all("a")
 
 filtered_headlines = []
 
 for headline in headlines:
     headline_text = headline.get_text().strip()
-    if headline_text and len(headline_text) > 25 and "CNN" not in headline_text and "cnn" not in headline_text:
+    if headline_text and len(headline_text) > 45 and "CNBC" not in headline_text and "cnbc" not in headline_text:
         filtered_headlines.append(headline_text)
         print(headline_text)
 
-with open("./static/scrap/cnn.json", "w", encoding="utf-8") as f:
+with open("./static/scrap/cnbc.json", "w", encoding="utf-8") as f:
     json.dump(filtered_headlines, f, indent=4, ensure_ascii=False)
