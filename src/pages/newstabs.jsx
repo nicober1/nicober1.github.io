29386e9a7591c8ac8ea6bb67f8a5ccd3b5487477
nicober1 <85498722+nicober1@users.noreply.github.com>
2023-05-06 15:56:45 +0000

    sss

---
+++ src/pages/newstabs.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
+function Card({ title }) {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg p-4">
+      <div className="font-bold text-xl mb-2">{title}</div>
+    </div>
+  );
+}
+
+function App() {
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);  
+  const [data3, setData3] = useState([]);
+
+
+  useEffect(() => {
+    // Replace with your actual API URLs
+    fetch("https://fluentblogs.com/scrap/cnn.json")
+      .then((response) => response.json())
+      .then((data) => setData1(data));
+
+      fetch("https://fluentblogs.com/scrap/cnbc.json")
+      .then((response) => response.json())
+      .then((data) => setData3(data));
+
+    fetch("https://fluentblogs.com/scrap/bbc.json")
+      .then((response) => response.json())
+      .then((data) => setData2(data));
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <Tabs>
+        <TabList>
+          <Tab>CNN</Tab>
+          <Tab>BBC</Tab>          
+          <Tab>CNBC</Tab>
+
+        </TabList>
+
+        <TabPanel>
+          <div className="grid grid-cols-3 gap-4">
+            {data1.map((item) => (
+              <Card title={item} key={item} />
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-3 gap-4">
+            {data2.map((item) => (
+              <Card title={item} key={item} />
+            ))}
+          </div>
+        </TabPanel>
+
+         <TabPanel>
+          <div className="grid grid-cols-3 gap-4">
+            {data3.map((item) => (
+              <Card title={item} key={item} />
+            ))}
+          </div>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+}
+
+export default App;
