d1f537b8e00847dd6b03fd690ba4199333bfeaa9
nicober1 <85498722+nicober1@users.noreply.github.com>
2023-05-07 07:06:33 +0000

    ss

--- src/pages/newstabs.jsx
+++ src/pages/newstabs.jsx
@@ -18,7 +18,17 @@ function Card({ title }) {
 function App() {
   const [data1, setData1] = useState([]);
   const [data2, setData2] = useState([]);  
-  const [data3, setData3] = useState([]);
+  const [data3, setData3] = useState([]);  
+  const [data4, setData4] = useState([]);  
+  const [data5, setData5] = useState([]);  
+  const [data6, setData6] = useState([]);
+  const [data7, setData7] = useState([]);
+  const [data8, setData8] = useState([]);
+  const [data9, setData9] = useState([]);
+  const [data10, setData10] = useState([]);
+  
+
+
 
 
   useEffect(() => {
@@ -26,14 +36,17 @@ function App() {
     fetch("https://fluentblogs.com/scrap/cnn.json")
       .then((response) => response.json())
       .then((data) => setData1(data));
-
-      fetch("https://fluentblogs.com/scrap/cnbc.json")
-      .then((response) => response.json())
-      .then((data) => setData3(data));
-
     fetch("https://fluentblogs.com/scrap/bbc.json")
       .then((response) => response.json())
       .then((data) => setData2(data));
+    fetch("https://fluentblogs.com/scrap/cnbc.json")
+      .then((response) => response.json())
+      .then((data) => setData3(data));
+
+    fetch("https://fluentblogs.com/scrap/mc.json")
+      .then((response) => response.json())
+      .then((data) => setData4(data));
+
   }, []);
 
   return (
@@ -45,9 +58,9 @@ function App() {
         <TabList>
           <Tab>CNN</Tab>
           <Tab>BBC</Tab>          
-          <Tab>CNBC</Tab>
-
-        </TabList>
+          <Tab>CNBC</Tab>          
+          <Tab>MoneyControl</Tab>
+      </TabList>
 
         <TabPanel>
           <div className="grid grid-cols-3 gap-4">
@@ -67,6 +80,14 @@ function App() {
          <TabPanel>
           <div className="grid grid-cols-3 gap-4">
             {data3.map((item) => (
+              <Card title={item} key={item} />
+            ))}
+          </div>
+        </TabPanel>
+
+        <TabPanel>
+          <div className="grid grid-cols-3 gap-4">
+            {data4.map((item) => (
               <Card title={item} key={item} />
             ))}
           </div>
